5ed5c5b6bff9a1fc41badfb440ec587651ebcf8a
Ivanfslee <ivanfslee@gmail.com>
2020-10-12 00:21:15 -0700

    Added coinpaprika link when coin is searched for. Added a check for coinsList to avoid making additional request to API.

--- app.js
+++ app.js
@@ -10,14 +10,15 @@ const fetchCoin = async (coinID) => {
 
 const input = document.querySelector('.search-input');
 
-const addCoinURL = (url, coinName) => {
+const addCoinURL = (url, coinName, coinID) => {
     const ul = document.querySelector('ul');
     const newLi = document.createElement('li');
     const anchorTag = document.createElement('a');
-    anchorTag.innerText = coinName;
-    anchorTag.href = url;
+    anchorTag.innerHTML = 
+        `<a href="${url}">${coinName}</a>
+        <a target="_blank" href="https://coinpaprika.com/coin/${coinID}">Coin Paprika Link</a>`
     newLi.append(anchorTag);
-    ul.append(newLi);    
+    ul.append(newLi);
 }
 
 let timeoutID;
@@ -32,15 +33,18 @@ input.addEventListener('input', (e) => {
 
     timeoutID = setTimeout(async () => {
 
-        //Get coin list
-        await fetchCoinList()
-            .then((res) => {
-                coinsList = res;
-            })
-            .catch((err) => {
-                console.log(err);
-            });
-        
+        //Get coin list, if coinsList not defined (saves us an additional HTML request)
+        if (!coinsList) {
+            coinsList = await fetchCoinList()
+            // TODO - error handling of fetchCoinList() call
+                // .then((res) => {
+                //     coinsList = res;
+                // })
+                // .catch((err) => {
+                //     console.log(err);
+                // });
+        }
+            
         //Iterate through coin list and find match to search term
         //If there is a match, then make another API call with coin_id
         for (let coin of coinsList) {
@@ -49,15 +53,17 @@ input.addEventListener('input', (e) => {
             if (coinName === searchTerm || coinSymbol === searchTerm) {
                 console.log(coin.id);
                 
-                await fetchCoin(coin.id)
-                    .then((res) => {
-                        console.log(res);
-                        console.log(res.config.url);
-                        addCoinURL(res.config.url, res.data.name);
-                    })
-                    .catch((err) => {
-                        console.log(err);
-                    });
+                let singleCoin = await fetchCoin(coin.id);
+                // TODO - error handling of fetchCoin() call
+                    // .then((res) => {
+                    //     console.log(res);
+                    //     console.log(res.config.url);
+                    //     addCoinURL(res.config.url, res.data.name);
+                    // })
+                    // .catch((err) => {
+                    //     console.log(err);
+                    // });
+                addCoinURL(singleCoin.config.url, singleCoin.data.name, coin.id);
             }
         }
     }, 1000);

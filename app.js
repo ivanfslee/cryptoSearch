47da5be9a17a2877fdc9711a0ee2b625ad0fa8f4
Ivanfslee <ivanfslee@gmail.com>
2020-10-21 15:13:55 -0700

    Coded autocomplete search.

--- app.js
+++ app.js
@@ -8,8 +8,6 @@ const fetchCoin = async (coinID) => {
     return coin;
 }
 
-const input = document.querySelector('.search-input');
-
 const addCoinURL = (url, coinName, coinID) => {
     const ul = document.querySelector('ul');
     const newLi = document.createElement('li');
@@ -20,6 +18,20 @@ const addCoinURL = (url, coinName, coinID) => {
     newLi.append(anchorTag);
     ul.append(newLi);
 }
+
+const root = document.querySelector('.autocomplete');
+root.innerHTML = `
+    <input class="search-input" />
+    <div class="dropdown">
+        <div class="dropdown-menu">
+            <div class="dropdown-content results"></div>
+        </div>
+    </div>
+`;
+
+const input = document.querySelector('.search-input');
+const dropdown = document.querySelector('.dropdown');
+const resultsWrapper = document.querySelector('.results');
 
 let timeoutID;
 let coinsList;
@@ -44,15 +56,19 @@ input.addEventListener('input', (e) => {
                 //     console.log(err);
                 // });
         }
-            
+        
+        dropdown.classList.remove('is-active');
+        resultsWrapper.innerHTML = '';
         //Iterate through coin list and find match to search term
         //If there is a match, then make another API call with coin_id
         for (let coin of coinsList) {
             const coinName = coin.name.toLowerCase();
             const coinSymbol = coin.symbol.toLowerCase();
             if (coinName === searchTerm || coinSymbol === searchTerm) {
+                dropdown.classList.add('is-active');
+
                 console.log(coin.id);
-                
+
                 let singleCoin = await fetchCoin(coin.id);
                 // TODO - error handling of fetchCoin() call
                     // .then((res) => {
@@ -63,8 +79,39 @@ input.addEventListener('input', (e) => {
                     // .catch((err) => {
                     //     console.log(err);
                     // });
-                addCoinURL(singleCoin.config.url, singleCoin.data.name, coin.id);
+
+                const option = document.createElement('a');
+                option.href = `https://coinpaprika.com/coin/${coin.id}`
+                option.target = '_blank';
+                option.classList.add('dropdown-item');
+                option.innerHTML = `
+                    ${singleCoin.data.name}
+                    (${singleCoin.data.rank});
+                `;
+
+                console.log('Single Coin data: ', singleCoin.data);
+                // When coin in dropdown is clicked, it will
+                // replace the input string with the coin name
+                // and hide the dropdown
+                option.addEventListener('click', () => {
+                    input.value = singleCoin.data.name;
+                    dropdown.classList.remove('is-active');
+                })
+
+                resultsWrapper.appendChild(option);
+                // addCoinURL(singleCoin.config.url, singleCoin.data.name, coin.id);
             }
         }
     }, 1000);
 })
+
+// Hides dropdown if user clicks anywhere not part of the dropdown
+document.addEventListener('click', event => {
+    console.log(event.target);
+    if (!root.contains(event.target)) {
+        dropdown.classList.remove('is-active');
+    }
+});
+
+//Documentation for bulma dropdown 
+    //https://bulma.io/documentation/components/dropdown/
